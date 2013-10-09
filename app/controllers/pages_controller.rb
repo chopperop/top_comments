efe69a4cc195f7f44a810cdb68507c577c32e979
David <david.wu712@gmail.com>
2013-10-09 01:54:50 -0400

    changed to two subreddits

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,12 +16,12 @@ class PagesController < ApplicationController
       con.adapter :em_http
     end
        
-    subreddits = ['all', 'drugs', 'AskReddit', 'IAmA', 'bestof', 'pettyrevenge', 'DoesAnybodyElse', 'WTF', 'aww', 'cringepics',  'JusticePorn', 'creepyPMs', 'gaming', 'Games', 'movies', 'funny', 'AdviceAnimals', 'pics', 'videos', 'gifs', 'todayilearned', 'science', 'askscience', 'YouShouldKnow', 'explainlikeimfive', 'trees', 'LifeProTips', 'sex', 'Fitness', 'lifehacks', 'politics', 'worldnews', 'news', 'TrueReddit', 'technology', 'Android', 'programming', 'apple', 'dmt']
-    subredditsRand = rand(0..39)
+    subreddits = ['all', 'drugs']
+    
     @title, @numComments, @url, @externalLink, @author, @comment, @points, @time, @subRand = Array.new(9){[]}
     
     for i in 0..0
-      @subRand[i] = subreddits[subredditsRand]
+      @subRand[i] = subreddits.sample
     
       rand = rand(0..24)
       @parentLink = reddit.get_listing(subreddit: @subRand[i], sort: 'hot')["data"]["children"][rand]["data"]
@@ -34,23 +34,23 @@ class PagesController < ApplicationController
       @link_id = @parentLink["id"]
     
     
-      # @firstParentComment = reddit.get_comments(link_id: @link_id, sort: "best", limit: 5)[1]["data"]["children"]
-#     
-#       if !@firstParentComment.empty?
-#         if @firstParentComment.length >= 2
-#           rand2 = rand(0..(@firstParentComment.length-2))
-#         else
-#           rand2 = 0
-#         end
-#         @parentComment = @firstParentComment[rand2]["data"]
-#         @author[i] = @parentComment["author"]
-#         @comment[i] = @parentComment["body"]
-#         @points[i] = @parentComment["ups"]
-#         @time[i] = DateTime.strptime(@parentComment["created_utc"].to_s, '%s').to_s
-#         
-#         @clicks.score += 1
-#         @clicks.save
-#       end
+      @firstParentComment = reddit.get_comments(link_id: @link_id, sort: "best", limit: 5)[1]["data"]["children"]
+    
+      if !@firstParentComment.empty?
+        if @firstParentComment.length >= 2
+          rand2 = rand(0..(@firstParentComment.length-2))
+        else
+          rand2 = 0
+        end
+        @parentComment = @firstParentComment[rand2]["data"]
+        @author[i] = @parentComment["author"]
+        @comment[i] = @parentComment["body"]
+        @points[i] = @parentComment["ups"]
+        @time[i] = DateTime.strptime(@parentComment["created_utc"].to_s, '%s').to_s
+        
+        @clicks.score += 1
+        @clicks.save
+      end
     end
     
   end
