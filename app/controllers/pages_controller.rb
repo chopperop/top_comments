2d34aeb8ef0e52afd6c912eb6ac1ec5d0b16707c
David <david.wu712@gmail.com>
2013-10-11 02:00:03 -0400

    testing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,8 +12,8 @@ class PagesController < ApplicationController
     
     @clicks = Comment.first
     
-    #@subRand = ['all', 'drugs', 'AskReddit', 'IAmA', 'bestof', 'pettyrevenge', 'DoesAnybodyElse', 'WTF', 'aww', 'cringepics',  'JusticePorn', 'creepyPMs', 'gaming', 'Games', 'movies', 'funny', 'AdviceAnimals', 'pics', 'videos', 'gifs', 'todayilearned', 'science', 'askscience', 'YouShouldKnow', 'explainlikeimfive', 'trees', 'LifeProTips', 'sex', 'Fitness', 'lifehacks', 'politics', 'worldnews', 'news', 'TrueReddit', 'technology', 'Android', 'programming', 'apple', 'dmt']
-    @subRand = 'all'
+    @subRand = ['all', 'drugs', 'AskReddit', 'IAmA', 'bestof', 'pettyrevenge', 'DoesAnybodyElse', 'WTF', 'aww', 'cringepics',  'JusticePorn', 'creepyPMs', 'gaming', 'Games', 'movies', 'funny', 'AdviceAnimals', 'pics', 'videos', 'gifs', 'todayilearned', 'science', 'askscience', 'YouShouldKnow', 'explainlikeimfive', 'trees', 'LifeProTips', 'sex', 'Fitness', 'lifehacks', 'politics', 'worldnews', 'news', 'TrueReddit', 'technology', 'Android', 'programming', 'apple', 'dmt'].sample
+    #@subRand = 'all'
     
     def reddit
       
@@ -26,14 +26,14 @@ class PagesController < ApplicationController
       rand = rand(0..4)
       parent = reddit.get_listing(subreddit: @subRand, sort: 'hot', limit: 5)["data"]["children"][rand]["data"]
       
-      @link_id = parent["id"]
-      
-      comment = reddit.get_comments(link_id: @link_id, sort: "best", limit: 2)[1]["data"]["children"]
+      # @link_id = parent["id"]
+#       
+#       comment = reddit.get_comments(link_id: @link_id, sort: "best", limit: 2)[1]["data"]["children"]
         
-      return parent, comment
+      # return parent, comment
     end
     
-    @parentLink, @firstParentComment = reddit
+    @parentLink = reddit# , @firstParentComment = reddit
     
     @title = @parentLink["title"]
     @numComments = @parentLink["num_comments"]
@@ -44,21 +44,21 @@ class PagesController < ApplicationController
       @externalLink = nil
     end
 
-    if !@firstParentComment.empty?
-      if @firstParentComment.length >= 2
-        rand2 = rand(0..(@firstParentComment.length-2))
-      else
-        rand2 = 0
-      end
-      @parentComment = @firstParentComment[rand2]["data"]
-      @author = @parentComment["author"]
-      @comment = @parentComment["body"]
-      @points = @parentComment["ups"]
-      @time = DateTime.strptime(@parentComment["created_utc"].to_s, '%s').to_s
-    
-      @clicks.score += 1
-      @clicks.save
-    end
+    # if !@firstParentComment.empty?
+#       if @firstParentComment.length >= 2
+#         rand2 = rand(0..(@firstParentComment.length-2))
+#       else
+#         rand2 = 0
+#       end
+#       @parentComment = @firstParentComment[rand2]["data"]
+#       @author = @parentComment["author"]
+#       @comment = @parentComment["body"]
+#       @points = @parentComment["ups"]
+#       @time = DateTime.strptime(@parentComment["created_utc"].to_s, '%s').to_s
+#     
+#       @clicks.score += 1
+#       @clicks.save
+#     end
     
   
   end
