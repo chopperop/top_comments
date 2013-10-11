83d7b394758fe10cba65b8c2db3f3c9166530fdf
David <david.wu712@gmail.com>
2013-10-10 23:05:58 -0400

    changed the controller and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,20 +20,20 @@ class PagesController < ApplicationController
     #@subreddit = Reddit.all.sample.subreddit   
     subreddits = ['all', 'drugs', 'AskReddit', 'IAmA', 'bestof', 'pettyrevenge', 'DoesAnybodyElse', 'WTF', 'aww', 'cringepics',  'JusticePorn', 'creepyPMs', 'gaming', 'Games', 'movies', 'funny', 'AdviceAnimals', 'pics', 'videos', 'gifs', 'todayilearned', 'science', 'askscience', 'YouShouldKnow', 'explainlikeimfive', 'trees', 'LifeProTips', 'sex', 'Fitness', 'lifehacks', 'politics', 'worldnews', 'news', 'TrueReddit', 'technology', 'Android', 'programming', 'apple', 'dmt']
   
-    @title, @numComments, @url, @externalLink, @author, @comment, @points, @time, @subRand = Array.new(9){[]}
     
-    for i in 0..0
-      #@subRand[i] = @subreddits.sample.subreddit
-      #@subRand[i] = @subreddit
-      @subRand[i] = subreddits.shuffle.first
+    
+    
+      #@subRand = @subreddits.sample.subreddit
+      #@subRand = @subreddit
+      @subRand = subreddits.shuffle.first
     
       rand = rand(0..4)
-      @parentLink = reddit.get_listing(subreddit: @subRand[i], sort: 'hot', limit: 5)["data"]["children"][rand]["data"]
-      @title[i] = @parentLink["title"]
-      @numComments[i] = @parentLink["num_comments"]
-      @url[i] = @parentLink["permalink"]
+      @parentLink = reddit.get_listing(subreddit: @subRand, sort: 'hot', limit: 5)["data"]["children"][rand]["data"]
+      
       if !@parentLink["url"].include?("reddit")
-        @externalLink[i] = @parentLink["url"]
+        @externalLink = @parentLink["url"]
+      else
+        @externalLink = nil
       end
       @link_id = @parentLink["id"]
     
@@ -47,15 +47,13 @@ class PagesController < ApplicationController
           rand2 = 0
         end
         @parentComment = @firstParentComment[rand2]["data"]
-        @author[i] = @parentComment["author"]
-        @comment[i] = @parentComment["body"]
-        @points[i] = @parentComment["ups"]
-        @time[i] = DateTime.strptime(@parentComment["created_utc"].to_s, '%s').to_s
+        
+        @time = DateTime.strptime(@parentComment["created_utc"].to_s, '%s').to_s
         
         @clicks.score += 1
         @clicks.save
       end
-    end
+    
     
   end
 
