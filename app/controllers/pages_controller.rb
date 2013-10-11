41e529e5200db392c3b13f2ea8dae2ae5bedabae
David <david.wu712@gmail.com>
2013-10-11 01:13:15 -0400

    changed to 5 and sample

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,9 +12,8 @@ class PagesController < ApplicationController
     
     @clicks = Comment.first
     
-    @subreddits = ['all', 'drugs', 'AskReddit', 'IAmA', 'bestof', 'pettyrevenge', 'DoesAnybodyElse', 'WTF', 'aww', 'cringepics',  'JusticePorn', 'creepyPMs', 'gaming', 'Games', 'movies', 'funny', 'AdviceAnimals', 'pics', 'videos', 'gifs', 'todayilearned', 'science', 'askscience', 'YouShouldKnow', 'explainlikeimfive', 'trees', 'LifeProTips', 'sex', 'Fitness', 'lifehacks', 'politics', 'worldnews', 'news', 'TrueReddit', 'technology', 'Android', 'programming', 'apple', 'dmt']
-    randReddit = rand(0..38)
-    @subRand = @subreddits[randReddit]
+    
+    @subRand = ['all', 'drugs', 'AskReddit', 'IAmA', 'bestof'].sample
     
     def reddit
       
