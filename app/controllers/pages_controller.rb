f3322f08e5e7756b9349a7d5ba8c4e4850e5f7c1
David <david.wu712@gmail.com>
2013-10-10 22:41:41 -0400

    changed back to the 30 subreddits

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,7 +18,7 @@ class PagesController < ApplicationController
     
     #@subreddits = Reddit.all   
     #@subreddit = Reddit.all.sample.subreddit   
-    subreddits = ['all']
+    subreddits = ['all', 'drugs', 'AskReddit', 'IAmA', 'bestof', 'pettyrevenge', 'DoesAnybodyElse', 'WTF', 'aww', 'cringepics',  'JusticePorn', 'creepyPMs', 'gaming', 'Games', 'movies', 'funny', 'AdviceAnimals', 'pics', 'videos', 'gifs', 'todayilearned', 'science', 'askscience', 'YouShouldKnow', 'explainlikeimfive', 'trees', 'LifeProTips', 'sex', 'Fitness', 'lifehacks', 'politics', 'worldnews', 'news', 'TrueReddit', 'technology', 'Android', 'programming', 'apple', 'dmt']
   
     @title, @numComments, @url, @externalLink, @author, @comment, @points, @time, @subRand = Array.new(9){[]}
     
