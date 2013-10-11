6e461cc8969677270ce0031f3660f04ed4431369
David <david.wu712@gmail.com>
2013-10-11 01:28:14 -0400

    added sample to array

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,21 +12,7 @@ class PagesController < ApplicationController
     
     @clicks = Comment.first
     
-    subredditsRand = rand(0..5)
-    case subredditsRand
-    when 0 
-      @subRand = 'all' #all and random
-    when 1 
-      @subRand = 'drugs'
-    when 2 
-      @subRand = 'AskReddit'
-    when 3 
-      @subRand = 'IAmA'
-    when 4 
-      @subRand = 'pettyrevenge'
-    when 5 
-      @subRand = 'DoesAnybodyElse'  
-    end  
+    @subRand = ['all', 'drugs'].sample
     
     def reddit
       
