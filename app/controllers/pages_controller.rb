32700fb7f22107eeca4dc805106a512034e95cfb
David <david.wu712@gmail.com>
2013-10-13 21:35:24 -0400

    changed it back to 10 controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,4 @@
 require 'rubygems'
-# require 'nokogiri'
-# require 'open-uri'
-# require 'mechanize'
-require 'httparty'
 require 'snoo'
 
 class PagesController < ApplicationController
@@ -15,58 +11,52 @@ class PagesController < ApplicationController
     
     @subreddits = ['all', 'drugs', 'AskReddit', 'IAmA', 'bestof', 'pettyrevenge', 'DoesAnybodyElse', 'WTF', 'aww', 'cringepics',  'JusticePorn', 'creepyPMs', 'gaming', 'Games', 'movies', 'funny', 'AdviceAnimals', 'pics', 'videos', 'gifs', 'todayilearned', 'science', 'askscience', 'YouShouldKnow', 'explainlikeimfive', 'trees', 'LifeProTips', 'sex', 'Fitness', 'lifehacks', 'politics', 'worldnews', 'news', 'TrueReddit', 'technology', 'Android', 'programming', 'apple', 'dmt']
 
-    @subRand = @subreddits[1]
-    #@subRand = 'all'
+    @subRand = @subreddits.shuffle.first
     
-    @html = "http://www.reddit.com/r/#{@subRand}/.json?limit=5" 
+    def reddit
+      
+      reddit = Snoo::Client.new do |con|
+        con.adapter :em_http
+      end
     
-    rand = rand(0..4)
-    @reddit = HTTParty.get(@html)["data"]["children"][rand]["data"]["title"]
+      
+  
+      rand = rand(0..4)
+      parent = reddit.get_listing(subreddit: @subRand, sort: 'hot', limit: 5)["data"]["children"][rand]["data"]
+      
+      @link_id = parent["id"]
+      
+      comment = reddit.get_comments(link_id: @link_id, sort: "best", limit: 2)[1]["data"]["children"]
+        
+      return parent, comment
+    end
     
-    # def reddit
- #      
- #      reddit = Snoo::Client.new do |con|
- #        con.adapter :em_http
- #      end
- #    
- #      
- #  
- #      rand = rand(0..4)
- #      parent = reddit.get_listing(subreddit: @subRand, sort: 'hot', limit: 5)["data"]["children"][rand]["data"]
- #      
- #      @link_id = parent["id"]
- #      
- #      comment = reddit.get_comments(link_id: @link_id, sort: "best", limit: 2)[1]["data"]["children"]
- #        
- #      return parent, comment
- #    end
- #    
- #    @parentLink, @firstParentComment = reddit
- #    
- #    @title = @parentLink["title"]
- #    @numComments = @parentLink["num_comments"]
- #    @url = @parentLink["permalink"]
- #    if !@parentLink["url"].include?("reddit")
- #      @externalLink = @parentLink["url"]
- #    else
- #      @externalLink = nil
- #    end
- # 
- #    if !@firstParentComment.empty?
- #      if @firstParentComment.length >= 2
- #        rand2 = rand(0..(@firstParentComment.length-2))
- #      else
- #        rand2 = 0
- #      end
- #      @parentComment = @firstParentComment[rand2]["data"]
- #      @author = @parentComment["author"]
- #      @comment = @parentComment["body"]
- #      @points = @parentComment["ups"]
- #      @time = DateTime.strptime(@parentComment["created_utc"].to_s, '%s').to_s
- #    
- #      @clicks.score += 1
- #      @clicks.save
- #    end
+    @parentLink, @firstParentComment = reddit
+    
+    @title = @parentLink["title"]
+    @numComments = @parentLink["num_comments"]
+    @url = @parentLink["permalink"]
+    if !@parentLink["url"].include?("reddit")
+      @externalLink = @parentLink["url"]
+    else
+      @externalLink = nil
+    end
+
+    if !@firstParentComment.empty?
+      if @firstParentComment.length >= 2
+        rand2 = rand(0..(@firstParentComment.length-2))
+      else
+        rand2 = 0
+      end
+      @parentComment = @firstParentComment[rand2]["data"]
+      @author = @parentComment["author"]
+      @comment = @parentComment["body"]
+      @points = @parentComment["ups"]
+      @time = DateTime.strptime(@parentComment["created_utc"].to_s, '%s').to_s
+    
+      @clicks.score += 1
+      @clicks.save
+    end
     
   
   end
