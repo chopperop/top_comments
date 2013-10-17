244afc900c316513269084b010480f725576018b
David <david.wu712@gmail.com>
2013-10-16 20:02:36 -0400

    added iron cache in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,7 @@ TopComments::Application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
   # config.cache_store = :dalli_store
+  config.cache_store = :iron_cache
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
